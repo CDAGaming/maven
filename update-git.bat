a430d9515bb890ea9986b8a630443f797c98f70c
deftware <opentexon@gmail.com>
2019-06-29 03:41:37 +0200

    Automatic push

--- update-git.bat
+++ update-git.bat
@@ -1,7 +1,6 @@
 @echo off
 node update-json
 node update-latest
-pause
 git add -A
 git commit -m "Automatic push"
 git push origin master
--- update-git.sh
+++ update-git.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 node update-json
 node update-latest
-read -p "Press enter to continue"
 git add -A
 git commit -m "Automatic push"
 git push origin master
